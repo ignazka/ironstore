e1076907f63093f5eca71fa5ca58ccb8d69b1b16
Philipp <picastle@posteo.de>
2021-09-08 14:59:45 +0200

    add list of products

--- script.js
+++ script.js
@@ -1,47 +1,43 @@
 //Variables, Arrays, and Objects, dotNotation, bracketNotation
 //Dom manipulation
 
-
 let items = [
-    {
-      name: 'Ironhack T',
-      price: 10,
-      image: 'https://miro.medium.com/max/5190/1*aVsUjp1zvlRb1799gDjbLA@2x.jpeg'
-    },
-    {
-      name: 'Ironhack Hoodie',
-      price: 15,
-      image: 'https://m.media-amazon.com/images/I/B1i3u9-Q-KS._AC_CLa%7C2140%2C2000%7CB1wqstnnTfS.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_UL1500_.png'
-    },
-    {
-      name: 'Ironhack Sticker',
-      price: 2,
-      image:'https://e7.pngegg.com/pngimages/887/803/png-clipart-ironhack-web-development-job-startup-company-design-blue-user-interface-design-thumbnail.png'
-    },
-    {
-      name: 'Ironhack Mug',
-      price: 8,
-      image: 'https://d0bb7f9bf11b5ad1a6b2-6175f06f5e3f64e15abbf67415a276ec.ssl.cf1.rackcdn.com/product-images/designlab/11-oz-traditional-ceramic-coffee-mugs-7102-white1582888132.jpg'
-    },
-  ]
+  {
+    name: "Ironhack T",
+    price: 10,
+    image: "https://miro.medium.com/max/5190/1*aVsUjp1zvlRb1799gDjbLA@2x.jpeg",
+  },
+  {
+    name: "Ironhack Hoodie",
+    price: 15,
+    image:
+      "https://m.media-amazon.com/images/I/B1i3u9-Q-KS._AC_CLa%7C2140%2C2000%7CB1wqstnnTfS.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_UL1500_.png",
+  },
+  {
+    name: "Ironhack Sticker",
+    price: 2,
+    image:
+      "https://e7.pngegg.com/pngimages/887/803/png-clipart-ironhack-web-development-job-startup-company-design-blue-user-interface-design-thumbnail.png",
+  },
+  {
+    name: "Ironhack Mug",
+    price: 8,
+    image:
+      "https://d0bb7f9bf11b5ad1a6b2-6175f06f5e3f64e15abbf67415a276ec.ssl.cf1.rackcdn.com/product-images/designlab/11-oz-traditional-ceramic-coffee-mugs-7102-white1582888132.jpg",
+  },
+];
 
+let list = document.querySelector("ul");
 
-let cart = []
+items.forEach((item) => {
+  list.innerHTML += `
+  <li>
+  <div>Name: ${item.name}</div>
+  <div>Price: $${item.price}</div>
+  <img src=${item.image}>
+  </li>`;
 
-
-items.forEach (item => {
-  console.log(item.name);
 });
-
-console.log("hello");
-
-
-
-
-
-
-
-
 
 /*
 let list = document.querySelector('#items')
@@ -59,7 +55,7 @@ items.forEach((item,i)=>{
 
 function showCart() {
     let cartItems = document.querySelector('#cart')
-    let grandTotal = 0; 
+    let grandTotal = 0;
     cartItems.innerHTML = ''
     cart.forEach((item,i)=>{
         grandTotal+= item.price * item.quantity
@@ -71,7 +67,7 @@ function showCart() {
     })
 
     document.querySelector('#grandTotal').innerHTML = '$' +grandTotal
- 
+
 }
 
 
